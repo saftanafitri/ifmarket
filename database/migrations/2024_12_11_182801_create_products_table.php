2d4bf86a85d48086556ee36a5ea8a0ea4574e9ba
rfauzannn <rizkcofauzana@gmail.com>
2026-02-09 14:57:01 +0700

    Initial Commit

---
+++ database/migrations/2024_12_11_182801_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {    
+            $table->id(); // 1
+            $table->string('name'); // Product name
+            $table->string('slug')->nullable()->unique();
+            $table->string('video')->nullable(); // String column for a single video path
+            $table->text('description'); // Product description
+            $table->string('seller_name'); // Seller name
+            $table->string('email'); // Seller email
+            $table->string('instagram')->nullable(); // Instagram link (nullable)
+            $table->string('linkedin')->nullable(); // LinkedIn link (nullable)
+            $table->string('github')->nullable(); // GitHub link (nullable)
+            $table->string('product_link'); // Product link (required)
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            // Menambahkan kolom user_id
+            $table->unsignedBigInteger('user_id'); // Kolom user_id untuk mengaitkan produk dengan user
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2025_05_30_155405_add_status_to_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('products', function ($table) {
+            $table->string('status')->default('pending')->after('column_name');
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
