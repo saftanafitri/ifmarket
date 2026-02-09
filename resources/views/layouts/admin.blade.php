58cb5ddd62fb51fd305ec128afc0b841e9e4e397
rfauzannn <rizkcofauzana@gmail.com>
2026-02-09 22:36:52 +0700

    tambah icon sakti produk

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Sakti Product')</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('images/Sakti.png') }}">
+    <title>@yield('title', 'Sakti Product')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     
     @stack('styles')
