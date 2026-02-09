c1af31cae584fed7cc1129668c5ec896ab3fb305
rfauzannn <rizkcofauzana@gmail.com>
2026-02-09 13:49:23 +0700

    untuk pengguna (mhs atau dosen)

---
+++ app/Http/Middleware/OnlyUser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OnlyUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+ 
+public function handle(Request $request, Closure $next)
+{
+    // Cek dulu apakah user sudah login
+    if (!Auth::check()) {
+        return redirect()->route('auth.login'); 
+    }
+
+    if (Auth::user()->role_id != 2) {
+        return redirect()->route('admin.dashboard'); 
+    }
+
+    // Jika user biasa, izinkan akses
+    return $next($request);
+}
+}
