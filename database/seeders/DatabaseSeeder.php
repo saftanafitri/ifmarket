c9b36954b3cab04f9046fd9878e8709dfd22787b
rfauzannn <rizkcofauzana@gmail.com>
2026-02-12 21:18:37 +0700

    nonaktifkan seeder produk

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             CategorySeeder::class,
-            // ProductSeeder::class,
+            //ProductSeeder::class,
         ]);
     }
 }
